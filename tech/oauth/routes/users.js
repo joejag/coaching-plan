d91c9d29831c7e5b476f728729832b79cc859bc2
Joe Wright <joseph.wright@ros.gov.uk>
2019-03-26 11:59:15 +0000

    Standard format everything

--- tech/oauth/routes/users.js
+++ tech/oauth/routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
+  res.send('some users')
+})
 
-module.exports = router;
+module.exports = router
