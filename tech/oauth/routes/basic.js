948e7f0a3c35dae3434438ca121a06f31b96e0ae
Joe Wright <joseph.wright@ros.gov.uk>
2019-03-26 12:24:59 +0000

    basic auth

---
+++ tech/oauth/routes/basic.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var router = express.Router()
+
+router.use((req, res, next) => {
+  const auth = { login: 'u', password: 'p' }
+
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+  if (!login || !password || login !== auth.login || password !== auth.password) {
+    res.set('WWW-Authenticate', 'Basic realm="401"')
+    res.status(401).send('Authentication required.')
+  } else {
+    next()
+  }
+})
+
+router.get('/', function (req, res, next) {
+  res.send('Users: Ya Basic')
+})
+
+module.exports = router
--- tech/oauth/routes/users.js
+++ tech/oauth/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.send('some users')
+  res.send('Users: Wide open')
 })
 
 module.exports = router
