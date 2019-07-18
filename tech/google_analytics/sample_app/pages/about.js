0fc98756df9a13087103b1a0b066d4b279c415ff
Joe Wright <joseph.wright@ros.gov.uk>
2019-07-18 14:37:55 +0100

    fragments

--- tech/google_analytics/sample_app/pages/about.js
+++ tech/google_analytics/sample_app/pages/about.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 
 const About = () => (
-  <div>
+  <>
     <Head>
       <title>About us</title>
     </Head>
     <p>This is the about 2223 page</p>
-  </div>
+  </>
 )
 
 export default About
